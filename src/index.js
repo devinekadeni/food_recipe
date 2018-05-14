9b43e7cef8514df947e14fba5fc70bc4f6f096d7
devinekadeni <devinekadeni@gmail.com>
2018-05-14 19:01:34 +0700

    added home page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers';
+import './index.css';
+import App from './containers/App';
+
+// const logger = createLogger();
+// const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
